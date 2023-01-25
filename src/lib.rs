ba4daa4a9559b8f0f50e4ca18ee3a3988fe74d1c
Michael Sarkisian <msarkisian1996@gmail.com>
2023-01-25 14:56:57 -0500

    refactor to lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,123 @@
+use rayon::prelude::*;
+use std::{
+    collections::{BinaryHeap, HashMap},
+    fs::read_to_string,
+    path::Path,
+};
+#[macro_use]
+extern crate lazy_static;
+
+const GLOVE_PATH: &str = "./datasets/glove.pruned.300d.txt";
+
+lazy_static! {
+    static ref WORD_VECS: HashMap<String, [f64; 300]> = load_word_vectors(GLOVE_PATH);
+}
+
+#[derive(PartialEq)]
+pub struct ScoredWord {
+    pub word: &'static str,
+    pub score: f64,
+}
+
+impl PartialOrd for ScoredWord {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        self.score.partial_cmp(&other.score)
+    }
+}
+
+impl Ord for ScoredWord {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.score.partial_cmp(&other.score).unwrap()
+    }
+}
+
+impl Eq for ScoredWord {}
+
+fn load_word_vectors<P: AsRef<Path>>(path: P) -> HashMap<String, [f64; 300]> {
+    let words = read_to_string(path).unwrap();
+    let mut word_vectors = HashMap::new();
+    for line in words.lines() {
+        let mut tokens = line.split(' ');
+        let word = tokens.next().unwrap();
+
+        let mut word_vec = [0f64; 300];
+        for (i, token) in tokens.enumerate() {
+            word_vec[i] = token.parse::<f64>().unwrap();
+        }
+
+        word_vectors.insert(word.to_string(), word_vec);
+    }
+    word_vectors
+}
+
+fn get_vector(target: &str) -> Option<&[f64; 300]> {
+    WORD_VECS.get(target)
+}
+
+pub fn find_closest_words(target: &str) -> Option<BinaryHeap<ScoredWord>> {
+    let this_word_vec = get_vector(&target)?;
+    Some(
+        WORD_VECS
+            .par_iter()
+            .map(|(word, vec)| ScoredWord {
+                word,
+                score: dot_product(this_word_vec, vec),
+            })
+            .collect(),
+    )
+}
+
+fn dot_product<const S: usize>(a: &[f64; S], b: &[f64; S]) -> f64 {
+    a.iter()
+        .zip(b.iter())
+        .map(|(a, b)| a * b)
+        .fold(0f64, |acc, prod| acc + prod)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn dot_product_test() {
+        assert_eq!(
+            dot_product(&[1f64, 3f64, -5f64], &[4f64, -2f64, -1f64]),
+            3f64
+        );
+        assert_eq!(
+            dot_product(&[1f64, 3f64, -5f64], &[1f64, 3f64, -5f64]),
+            35f64
+        );
+    }
+
+    #[test]
+    fn heap_ordering() {
+        let mut heap = BinaryHeap::new();
+        heap.push(ScoredWord {
+            word: "a",
+            score: 0f64,
+        });
+        heap.push(ScoredWord {
+            word: "b",
+            score: 5f64,
+        });
+        heap.push(ScoredWord {
+            word: "c",
+            score: 2f64,
+        });
+        heap.push(ScoredWord {
+            word: "d",
+            score: 9f64,
+        });
+        heap.push(ScoredWord {
+            word: "e",
+            score: 0.030000540530,
+        });
+        heap.push(ScoredWord {
+            word: "f",
+            score: -0.030000540530,
+        });
+
+        assert_eq!(heap.pop().unwrap().score, 9f64);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,13 @@
 #![feature(binary_heap_into_iter_sorted)]
 
-use rayon::prelude::*;
-use std::{
-    collections::{BinaryHeap, HashMap},
-    fs::read_to_string,
-    io,
-};
-
-#[macro_use]
-extern crate lazy_static;
-
-const GLOVE_PATH: &str = "./datasets/glove.pruned.300d.txt";
-
-lazy_static! {
-    static ref WORD_VECS: HashMap<String, [f64; 300]> = load_word_vectors();
-}
-
-#[derive(PartialEq)]
-struct ScoredWord {
-    word: &'static str,
-    score: f64,
-}
-
-impl PartialOrd for ScoredWord {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        self.score.partial_cmp(&other.score)
-    }
-}
-
-impl Ord for ScoredWord {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        self.score.partial_cmp(&other.score).unwrap()
-    }
-}
-
-impl Eq for ScoredWord {}
+use mitten;
+use std::io;
 
 fn main() {
     loop {
         let mut word = String::new();
         io::stdin().read_line(&mut word).unwrap();
-        if let Some(closest_words) = find_closest_words(&word.trim()) {
+        if let Some(closest_words) = mitten::find_closest_words(&word.trim()) {
             closest_words
                 .into_iter_sorted()
                 .take(50)
@@ -48,94 +15,5 @@ fn main() {
             continue;
         }
         eprintln!("provided word not in dataset")
-    }
-}
-
-fn load_word_vectors() -> HashMap<String, [f64; 300]> {
-    let words = read_to_string(GLOVE_PATH).unwrap();
-    let mut word_vectors = HashMap::new();
-    for line in words.lines() {
-        let mut tokens = line.split(' ');
-        let word = tokens.next().unwrap();
-
-        let mut word_vec = [0f64; 300];
-        for (i, token) in tokens.enumerate() {
-            word_vec[i] = token.parse::<f64>().unwrap();
-        }
-
-        word_vectors.insert(word.to_string(), word_vec);
-    }
-    word_vectors
-}
-
-fn get_vector(target: &str) -> Option<&[f64; 300]> {
-    WORD_VECS.get(target)
-}
-
-fn find_closest_words(target: &str) -> Option<BinaryHeap<ScoredWord>> {
-    let this_word_vec = get_vector(&target)?;
-    Some(
-        WORD_VECS
-            .par_iter()
-            .map(|(word, vec)| ScoredWord {
-                word,
-                score: dot_product(this_word_vec, vec),
-            })
-            .collect(),
-    )
-}
-
-fn dot_product<const S: usize>(a: &[f64; S], b: &[f64; S]) -> f64 {
-    a.iter()
-        .zip(b.iter())
-        .map(|(a, b)| a * b)
-        .fold(0f64, |acc, prod| acc + prod)
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    fn dot_product_test() {
-        assert_eq!(
-            dot_product(&[1f64, 3f64, -5f64], &[4f64, -2f64, -1f64]),
-            3f64
-        );
-        assert_eq!(
-            dot_product(&[1f64, 3f64, -5f64], &[1f64, 3f64, -5f64]),
-            35f64
-        );
-    }
-
-    #[test]
-    fn heap_ordering() {
-        let mut heap = BinaryHeap::new();
-        heap.push(ScoredWord {
-            word: "a",
-            score: 0f64,
-        });
-        heap.push(ScoredWord {
-            word: "b",
-            score: 5f64,
-        });
-        heap.push(ScoredWord {
-            word: "c",
-            score: 2f64,
-        });
-        heap.push(ScoredWord {
-            word: "d",
-            score: 9f64,
-        });
-        heap.push(ScoredWord {
-            word: "e",
-            score: 0.030000540530,
-        });
-        heap.push(ScoredWord {
-            word: "f",
-            score: -0.030000540530,
-        });
-
-        assert_eq!(heap.pop().unwrap().score, 9f64);
     }
 }
