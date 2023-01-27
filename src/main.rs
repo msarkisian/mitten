c52f6cb8734fcbb2032af291c552c551d4f328d2
Michael Sarkisian <msarkisian1996@gmail.com>
2023-01-26 19:49:08 -0500

    update main.rs for new func name

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     loop {
         let mut word = String::new();
         io::stdin().read_line(&mut word).unwrap();
-        if let Some(closest_words) = mitten::find_closest_words(&word.trim()) {
+        if let Some(closest_words) = mitten::calculate_word_distance_from_word(&word.trim()) {
             closest_words
                 .into_iter_sorted()
                 .take(50)
